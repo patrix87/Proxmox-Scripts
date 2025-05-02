b12b30d8fa4f52f979fd030bf8d2e4c168a81e2b
Patrick Veilleux <patrix87@gmail.com>
2025-05-01 21:58:18 -0400

    add PBS fix

--- setup-script.sh
+++ setup-script.sh
@@ -25,10 +25,17 @@ cat <<EOF >/etc/apt/sources.list.d/pve-install-repo.list
 deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription
 EOF
 
+# Disabling 'pbs-enterprise' repository
+cat <<EOF >/etc/apt/sources.list.d/pbs-enterprise.list
+# deb https://enterprise.proxmox.com/debian/pbs bookworm pbs-enterprise
+EOF
+
 # Adding 'pbs-no-subscription' repository
-cat <<EOF >/etc/apt/sources.list.d/pbs-no-subscription.list
+cat <<EOF >/etc/apt/sources.list.d/pbs-install-repo.list
 deb http://download.proxmox.com/debian/pbs bookworm pbs-no-subscription
 EOF
+
+
 
 # Install Proxmox Backup Server
 apt-get update
@@ -45,6 +52,11 @@ EOF
 # Adding 'pvetest' repository and set disabled
 cat <<EOF >/etc/apt/sources.list.d/pvetest-for-beta.list
 # deb http://download.proxmox.com/debian/pve bookworm pvetest
+EOF
+
+# Adding 'pbstest' repository and set disabled
+cat <<EOF >/etc/apt/sources.list.d/pbstest-for-beta.list
+# deb http://download.proxmox.com/debian/pbs bookworm pbstest
 EOF
 
 # Disabling subscription nag
